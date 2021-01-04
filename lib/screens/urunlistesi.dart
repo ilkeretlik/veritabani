d8e232d10a3338ac7ec98d75a5098e3a807db3a6
ilkeretlik@hotmail.com <ilkeretlik@hotmail.com>
2021-01-04 21:29:07 +0300

    ürün listesi

--- lib/screens/urunlistesi.dart
+++ lib/screens/urunlistesi.dart
@@ -2,32 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:veritabani/db/dbHelper.dart';
 import 'package:veritabani/models/product.dart';
 
+
+
 class UrunListesi extends StatefulWidget {
   @override
   _UrunListesiState createState() => _UrunListesiState();
 }
 
 class _UrunListesiState extends State<UrunListesi> {
-DbHelper dbhelpersinifi = new DbHelper();
-List<Product> urunler ;
-int count=0;
+  DbHelper dbhelpersinifi = new DbHelper();
+  List<Product> urunler;
+  int count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-body:UrunItems(),
-
+      body: UrunItems(),
     );
   }
 
   UrunItems() {
-return ListView.builder(
-  
-  itemBuilder: null);
-
+    return ListView.builder(itemBuilder: null);
   }
 
+  void getData() {
+    var dbfuture = dbhelpersinifi.initializeDb();
+    dbfuture.then((result) {
+      // veritabanını aç
 
+      var gelenurunler = dbhelpersinifi.urunListele();
+      gelenurunler.then((result) {
+        List<Product> productData = new List<Product>();
+        count = result.length;
+        for (int i = 0; i < count; i++) {
 
+          productData.add(Product);
+
+        }
+      });
+    });
+  }
 }
